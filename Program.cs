bbdf1607aa071747f58ed47ddb1e090a8237603d
Snehal979 <bansodsnehal535@gmail.com>
2022-10-18 15:41:58 +0530

    employee wage

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeWag_4;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employeebuilding empWage = new Employeebuilding();
+
+            empWage.addCompanyEmpWage("jio", 30, 7, 10);
+            empWage.addCompanyEmpWage("Reilance", 20, 6, 20);
+
+            empWage.computeEmpWage();
+            Console.WriteLine("Total Wage for DMart company: " + empWage.getTotalEmpWage(0));
+        }
+    }
+}
